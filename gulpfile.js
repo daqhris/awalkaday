6d6f0c350d6190d7c9dea663ae53ab0712192124
Chris A. Iradukunda <daqhris@hotmail.com>
2021-06-01 23:58:30 +0200

    fix image display issue and correct dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -7,7 +7,7 @@ var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
 var del = require('del');
 
-gulp.task('resize', function () {
+gulp.task('resize', gulp.series(function () {
     return gulp.src('images/*.*')
         .pipe(imageResize({
             width: 1024,
@@ -19,35 +19,35 @@ gulp.task('resize', function () {
             imageMagick: true
         }))
         .pipe(gulp.dest('images/thumbs'));
-});
+}));
 
-gulp.task('del', ['resize'], function () {
+gulp.task('del', gulp.series('resize', function () {
     return del(['images/*.*']);
-});
+}));
 
 // compile scss to css
-gulp.task('sass', function () {
+gulp.task('sass', gulp.series(function () {
     return gulp.src('./assets/sass/main.scss')
         .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
         .pipe(rename({basename: 'main.min'}))
         .pipe(gulp.dest('./assets/css'));
-});
+}));
 
 // watch changes in scss files and run sass task
-gulp.task('sass:watch', function () {
+gulp.task('sass:watch', gulp.series(function () {
     gulp.watch('./assets/sass/**/*.scss', ['sass']);
-});
+}));
 
 // minify js
-gulp.task('minify-js', function () {
+gulp.task('minify-js', gulp.series(function () {
     return gulp.src('./assets/js/main.js')
         .pipe(uglify())
         .pipe(rename({basename: 'main.min'}))
         .pipe(gulp.dest('./assets/js'));
-});
+}));
 
 // default task
-gulp.task('default', ['del']);
+gulp.task('default', gulp.series('del'));
 
 // scss compile task
-gulp.task('compile-sass', ['sass', 'minify-js']);
+gulp.task('compile-sass', gulp.series('sass', 'minify-js'));

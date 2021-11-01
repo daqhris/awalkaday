56bb07a3dd3c1226ce39ed5f1fe59a22421ef09c
Chris-Armel <info@daqhris.com>
2021-11-01 22:40:01 +0100

    Test detection of web3 wallet app

--- assets/js/main.js
+++ assets/js/main.js
@@ -297,3 +297,27 @@
 	});
 
 })(jQuery);
+
+//Connect to a Web3 wallet app
+//Detect both the mobile and extension provider (window.ethereum).
+if (window.ethereum) {
+	handleEthereum();
+  } else {
+	window.addEventListener('ethereum#initialized', handleEthereum, {
+	  once: true,
+	});
+  
+	// If the event is not dispatched by the end of the timeout,
+	// the user probably doesn't have MetaMask installed.
+	setTimeout(handleEthereum, 3000); // 3 seconds
+  }
+  
+  function handleEthereum() {
+	const { ethereum } = window;
+	if (ethereum && ethereum.isMetaMask) {
+	  console.log('Ethereum successfully detected!');
+	  // Access the decentralized web!
+	} else {
+	  console.log('Please install MetaMask!');
+	}
+  }

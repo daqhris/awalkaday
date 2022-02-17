3aae7ca8db4f4ab08fcd730e14ab298be3813da0
Chris-Armel <info@daqhris.com>
2022-02-17 18:20:52 +0100

    update skel breakpoint values

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,6 +8,7 @@
 
 	skel.breakpoints({
 		large: '(max-width: 1680px)',
+		large: '(max-width: 1280px)',
 		medium: '(max-width: 980px)',
 		small: '(max-width: 736px)',
 		xsmall: '(max-width: 480px)'

68f1a5ef17f84a1266cb34abe536e3c0f7aa2995
Chris-Armel <info@daqhris.com>
2023-01-28 15:23:23 +0100

    Miscellaneous changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,8 +10,8 @@
 		large: '(max-width: 1680px)',
 		large: '(max-width: 1280px)',
 		medium: '(max-width: 980px)',
-		small: '(max-width: 736px)',
-		xsmall: '(max-width: 480px)'
+		small: '(max-width: 600px)',
+		xsmall: '(max-width: 320px)'
 	});
 
 	$(function() {
